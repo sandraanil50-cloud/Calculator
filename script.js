a8adb7ea0d18d8c3d937563490cdf64d7707b618
sandraanil50-cloud <sandraanil50@gmail.com>
2025-12-17 14:12:59 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function clearScreen() {
+    document.getElementById('result').value = '';
+}
+
+function deleteLast() {
+    let result = document.getElementById('result').value;
+    document.getElementById('result').value = result.slice(0, -1);
+}
+
+function appendToDisplay(value) {
+    document.getElementById('result').value += value;
+}
+
+function calculate() {
+    try {
+        let result = eval(document.getElementById('result').value);
+        document.getElementById('result').value = result;
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+    }
+}
